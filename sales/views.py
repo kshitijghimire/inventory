ec7c441d5b0cc82de8cf4d13af99be62e76009c4
Kshitij Ghimire <kshitijnep13@gmail.com>
2024-02-29 13:56:58 +0545

    work on sales  page

--- sales/views.py
+++ sales/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import sales
+
 
 # Create your views here.
 def saleshome(request):
-    return render(request,'sales.html')
+    context = {
+        't':'Sales Management',
+        'sales':sales.objects.all()
+    }
+    return render(request,'sales.html',context)
 
