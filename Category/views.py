5648704957f70a028badfc85d81ac36a8bbcc256
Kshitij Ghimire <kshitijnep13@gmail.com>
2024-03-03 16:39:49 +0545

    save edit delete work on Category form.py,savecategory.py,view.py,categories.html

--- Category/views.py
+++ Category/views.py
@@ -1,14 +1,43 @@
 from django.shortcuts import render
 from .models import Category
+from .forms import CategoryForm
 
 
 # Create your views here.
 def CategoryHome(request):
-     context = {
+    form = CategoryForm()
+
+
+    if request.method == 'POST':
+        if 'save' in request.POST:
+           
+            if request.POST.get('save'):
+                pk = request.POST.get('save')
+                item = Category.objects.get(id=pk)
+                items = CategoryForm(request.POST,instance=item)
+            else: 
+                items = CategoryForm(request.POST)
+            items.save()
+
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            item = Category.objects.filter(id=pk)
+            item.delete()
+
+        elif 'edit' in request.POST:
+            pk = request.POST.get('edit')
+            item = Category.objects.get(id=pk)
+            form = CategoryForm(instance=item)   
+
+    
+    
+  
+    context = {
+        'forms': form, 
         't':'Category Management',
         'Category':Category.objects.all()
 
 
 
     }
-     return render(request,'categories.html',context)
+    return render(request,'categories.html',context)
