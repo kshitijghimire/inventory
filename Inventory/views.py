3156966564e6ff88980e3cacddd34f6dc5246618
Kshitij Ghimire <kshitijnep13@gmail.com>
2024-03-03 17:57:32 +0545

    final work on inventory edit save and delete button

--- Inventory/views.py
+++ Inventory/views.py
@@ -9,6 +9,28 @@ from .forms import InventoryForm
 def InventoryHome(request):
     form = InventoryForm()
 
+    if request.method == 'POST':
+        if 'save' in request.POST:
+           
+            if request.POST.get('save'):
+                pk = request.POST.get('save')
+                item = Inventory.objects.get(id=pk)
+                items = InventoryForm(request.POST,instance=item)
+            else: 
+                items = InventoryForm(request.POST)
+            items.save()
+
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            item = Inventory.objects.filter(id=pk)
+            item.delete()
+
+        elif 'edit' in request.POST:
+            pk = request.POST.get('edit')
+            item = Inventory.objects.get(id=pk)
+            form = InventoryForm(instance=item)   
+
+
     context ={
         'forms': form, 
         't':'Inventory management',
